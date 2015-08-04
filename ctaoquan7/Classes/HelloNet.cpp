9551122fa3b45f0f0f3bf353670e99a0d547ef35
lizhengy81@gmail.com <lizhengy81@gmail.com>
2015-08-04 10:44:16 +0800

    tst

--- ctaoquan7/Classes/HelloNet.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  HelloNet.cpp
-//  ctaoquan7
-//
-//  Created by lizhengyan on 15/7/9.
-//
-//
-
-#include "HelloNet.h"
--- ctaoquan7/Classes/NoteDAO.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  NoteDAO.cpp
-//  ctaoquan7
-//
-//  Created by lizhengyan on 15/7/9.
-//
-//
-
-#include "NoteDAO.h"
